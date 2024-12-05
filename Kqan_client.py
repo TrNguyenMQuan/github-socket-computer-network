6d0f3b48d958f7f5c521adeafeac860dcb67717b
Minh Quan <trnguyenmquan@gmail.com>
2024-12-05 22:30:04 +0700

    Delete redundancy

--- Kqan_client.py
+++ Kqan_client.py
@@ -12,19 +12,11 @@ CHUNKS_FILE = 1024
 
 pending_file = []
 
-
 def scanInputAfter5Secs(source_file_name):
     position = 0
     while True:
         with open(source_file_name, "r") as file:
             file.seek(position)
-            # while True:
-            #     data = file.readline()
-            #     data = data.strip("\n")
-            #     if not data:
-            #         break
-            #     pending_file.append(data)
-            #     position = file.tell()
             while True:
                 data = file.readline()
                 if not data:
@@ -52,18 +44,6 @@ def runClient():
     print(f"connect successfully!")
     print(client.recv(1024).decode("utf_8"))
     
-    # with open(DATA_FILE_NAME, "wb") as file:
-    #     file_size = client.recv(1024).decode("utf_8")
-    #     # print(file_size)
-    #     file_data = client.recv(int(file_size))
-    #     file.write(file_data)
-
-    # # -----------------------------------------------------
-    # for count in range(20):
-    #     pending_files = getDownloadList(DOWNLOAD_FILE_NAME)
-    #     print(pending_files)
-    #     time.sleep(5)
-    # client.close()
 
     threading.Thread(target=scanInputAfter5Secs, daemon=False, args=(DOWNLOAD_FILE_NAME,)).start()
     threading.Thread(target=downloadFile, daemon=False, args=(client, )).start()
