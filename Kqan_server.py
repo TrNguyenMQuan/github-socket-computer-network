8c869621c974d24de363c75e47e0625a9f216f4a
ptn <phamthanhnam1346@gmail.com>
2024-12-06 20:11:20 +0700

    update

--- Kqan_server.py
+++ Kqan_server.py
@@ -1,10 +1,21 @@
 import os 
 import socket
+import keyboard
+
 DIRECTORY_OF_DATA = ".\Data"
 DATA_FILE_NAME = "list of files name.txt"
+HOST = "127.0.0.1"
+PORT = 9999
+FORMAT = "utf-8"
+SIZE = 1024
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(script_dir, DIRECTORY_OF_DATA)
+os.chdir(path)
+
+print(os.getcwd())
 
 
-# -----------------------------------------------------
 def convertFileSize(file_size):
     unit = ""
     if file_size < 1024:
@@ -33,45 +44,43 @@ def printListFile(source_file_name, list_file):
             for key, value in file.items():
                 f.write(f"{key}     {value}\n")
         
-# -----------------------------------------------------
-
 
 def runServer(HOST, PORT):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
-    server.listen(1)
+    server.listen()
     print("Server is runing... ")
 
     client_socket, addr = server.accept()
-    print(f"connected to {addr}")
-    client_socket.send(f"Hello, you are connecting to server at {(HOST, PORT)}".encode("utf_8"))
+    try:
+        while True:
+            file_requested = client_socket.recv(SIZE).decode(FORMAT)
+            
+            if not file_requested:
+                print("Client disconnected")
+                break
 
-    
-    # this code is used to combine with the main code
-    list_file = listFileInPath(DIRECTORY_OF_DATA)
-    
-    printListFile(DATA_FILE_NAME, list_file)
-    
-    with open(DATA_FILE_NAME, "rb") as file:
-        file_size = os.path.getsize(DATA_FILE_NAME)
-        file_data = file.read()
-        client_socket.send(str(file_size).encode("utf_8"))
-        client_socket.sendall(file_data)
+            file_size = os.path.getsize(file_requested)
+            client_socket.send(str(file_size).encode(FORMAT))
+            print(f"Client requested file: {file_requested} with {file_size}")
 
-    print(client_socket.recv(1024).decode("utf_8"))
+            with open(file_requested, "rb") as file:
+                while True:
+                    data_send = file.read(SIZE)
 
-    client_socket.close()
-    server.close()
-    
+                    if not data_send:
+                        break
+                    
+                    client_socket.sendall(data_send)
+
+            print(f"File : {file_requested} sended successfully \n")
+    except KeyboardInterrupt:
+        client_socket.close()
+    finally:
+        client_socket.close()
 
 def main():
-    # directory_path = '.\data'
-    # list_file = listFileInPath(directory_path)
-    # for file in list_file:
-    #     for key, value in file.items():
-    #         print(f"{key} {value}")
-
-    runServer("localhost", 9999)
+    runServer(HOST, PORT)
 
 if __name__ == "__main__":
     main()
